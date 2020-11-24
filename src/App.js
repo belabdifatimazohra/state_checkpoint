f1b20e8cc9a13fdbe17f42603e97da711f4529b1
belabdifatimazohra <fatima.zohra.belabdi@gmail.com>
2020-11-24 10:04:23 +0000

    toggle button to show or hide profile

--- src/App.js
+++ src/App.js
@@ -22,10 +22,11 @@ class App extends React.Component {
   };
 
   componentDidMount() {
+    // After 1 seconde increment counter +1
     setInterval(this.incrementCounter, 1000);
   }
-  // Show Profile
-  handleClick = () => this.setState({ show: true })
+  // Show or Hide Profile
+  handleClick = () => this.setState({ show: !this.state.show })
 
   render() {
     const styleProfile = {
@@ -36,11 +37,13 @@ class App extends React.Component {
 
     return (
       <>
-        {/* If Show is true show the profile and not the button of show the profile */}
+
         <div className="show">
           <h1>The component has mounted</h1>
           <h2>{this.state.counter}</h2>
-          {(this.state.show) ?
+          {/* If Show is true show the profile and the button set to Hide the profile else do the opposite */}
+          <button onClick={this.handleClick}> {this.state.show ? 'Hide Profile' : 'Show Profile'} </button>
+          {(this.state.show) &&
             <div style={styleProfile}>
               <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', justifyItems: 'center', textAlign: 'center' }}>
                 <div>
@@ -52,8 +55,6 @@ class App extends React.Component {
                 <span> {this.state.Person.bio} </span>
               </div>
             </div>
-            // Else show the button Show Profile
-            : <button onClick={this.handleClick}> Show Profile </button>
           }
         </div>
 
