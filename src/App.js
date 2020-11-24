097e3490898ab1022b1cb7cd673b46b8b32b8cdd
belabdifatimazohra <fatima.zohra.belabdi@gmail.com>
2020-11-24 08:45:45 +0000

    show profile hide button

--- src/App.js
+++ src/App.js
@@ -36,12 +36,11 @@ class App extends React.Component {
 
     return (
       <>
+        {/* If Show is true show the profile and not the button of show the profile */}
         <div className="show">
           <h1>The component has mounted</h1>
           <h2>{this.state.counter}</h2>
-          <button onClick={this.handleClick}> Show Profile </button>
-
-          {(this.state.show) &&
+          {(this.state.show) ?
             <div style={styleProfile}>
               <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', justifyItems: 'center', textAlign: 'center' }}>
                 <div>
@@ -53,6 +52,8 @@ class App extends React.Component {
                 <span> {this.state.Person.bio} </span>
               </div>
             </div>
+            // Else show the button Show Profile
+            : <button onClick={this.handleClick}> Show Profile </button>
           }
         </div>
 
